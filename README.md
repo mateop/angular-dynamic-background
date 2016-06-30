7ec770e9aeaaf971e6de72e3cc0c0b3aaec7e423
mateop <percinic@gmail.com>
2016-06-30 13:58:19 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,13 @@
+# Angular Dynamic Background
+
+Angular Dynamic Background is a simple agularjs directive that changes the background color of an element and sets an optimal font color for better legibility.
+
+## Installation
+For the latest stable version, use Bower from the command line.
+```bash
+bower install angular-dynamic-background --save
+```
+## Usage
+```html
+<any dynamic-background="'#FF0000'"></any>
+```
---
+++ src/angularDynamicBackground.directive.js
@@ -0,0 +1,82 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('dynamicBackground')
+        .directive('dynamicBackground', function () {
+            return {
+                scope: {
+                    dynamicBackground: '='
+                },
+                restrict: 'A',
+                link: function (scope, element, attr) {
+                    var rgb;
+                    scope.$watch('dynamicBackground', function (newValue) {
+                        
+                        if(typeof newValue == 'undefined'){
+                            return;
+                        }
+                        var hexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+                        var rgbRegex = /^(rgb|hsl)(a?)[(]\s*([\d.]+\s*%?)\s*,\s*([\d.]+\s*%?)\s*,\s*([\d.]+\s*%?)\s*(?:,\s*([\d.]+)\s*)?[)]$/;
+                        var error = false;
+                       
+                            if (hexRegex.test(newValue)) {
+                                rgb = hexToRGB(newValue);
+                            } else if (rgbRegex.test(newValue)) {
+                                rgb = rgbToArray(newValue);
+                            } else {
+                                error = true;
+                            }
+                    
+
+                        if (!error) {
+                            run();
+                        }
+                    });
+
+                    function run() {
+                        /*if (window.Worker) {
+                            //calculate in worker
+                        } else {*/
+                            //calculate on main thread
+                            element.css('color', calculateColor());
+                            element.css('backgroundColor', 'rgb(' + rgb.join(',') +')');
+                       /* }*/
+                    }
+
+                    function calculateColor() {
+                        var c = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+                        var o = Math.round(((parseInt(rgb[0]) * 299) + (parseInt(rgb[1]) * 587) + (parseInt(rgb[2]) * 114)) / 1000);
+
+                        if (o > 125) {
+                            return '#000';
+                        } else {
+                            return '#fff';
+                        }
+                    }
+
+                    function hexToRGB(hex) {
+                        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+                        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+                        hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+                            return r + r + g + g + b + b;
+                        });
+
+                        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+                        return result ? [
+                             parseInt(result[1], 16),
+                             parseInt(result[2], 16),
+                             parseInt(result[3], 16)
+                        ] : null;
+                    }
+
+                    function rgbToArray(rgb){
+                        return rgb.substring(4, rgb.length-1)
+                            .replace(/ /g, '')
+                            .split(',');
+                    }
+                }
+            };
+        });
+
+})();
